7294663be534eb6184ba8c336e628ecc5b13b884
Aprizza <56188101+Aprizza@users.noreply.github.com>
2020-12-02 00:21:25 +0700

    Add files via upload

---
+++ data/MAKANAN_1.js
@@ -0,0 +1,8 @@
+var json_MAKANAN_1 = {
+"type": "FeatureCollection",
+"name": "MAKANAN_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.152537975338831, -6.112998560514907 ] } }
+]
+}
